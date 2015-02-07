a7d226eb62ab264007962747704a57da522df99c
fd0 <booleanlabel@gmail.com>
2015-02-07 22:55:02 +0900

    Fixed validator/pkgconfig

--- lib/xezat/validator/pkgconfig.rb
+++ lib/xezat/validator/pkgconfig.rb
@@ -6,17 +6,29 @@ module Xezat
       ValidatorManager::register(:pkgconfig ,self)
       def validate(variables)
         pcdir = File::join(variables[:D], 'usr', 'lib', 'pkgconfig')
-        result = false
+        result = true
+        details = []
         if Dir::exists?(pcdir)
           Find::find(pcdir) do |file|
             if file.end_with?('.pc')
-              result = result || File::open(file).read().index('@').nil?
+              File::foreach(file) { |line|
+                unless line.index('@').nil?
+                  result = false
+                  details << "#{file.gsub(variables[:D], '')}: contains @"
+                  break
+                end
+                if line.start_with?('Libs:') && line.index(' -l').nil?
+                  result = false
+                  details << "#{file.gsub(variables[:D], '')}: no library flags found"
+                  break
+                end
+              }
             end
           end
         else
           result = nil
         end
-        [result, nil]
+        [result, details.empty? ? nil : details.join($/)]
       end
     end
   end
