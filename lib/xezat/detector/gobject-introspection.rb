2564839960f390e2818120cf71dcd2b5ab5d0382
fd0 <booleanlabel@gmail.com>
2015-03-06 23:33:50 +0900

    Added detectors/gobject-introspection

---
+++ lib/xezat/detector/gobject-introspection.rb
@@ -0,0 +1,20 @@
+require 'find'
+require 'xezat/detectors'
+
+module Xezat
+  module Detector
+    class GobjectIntrospection
+      DetectorManager::register(:'gobject-introspection', self)
+      def detect(variables)
+        Find::find(variables[:S]) do |file|
+          if file.end_with?(File::SEPARATOR + 'configure.ac') || file.end_with?(File::SEPARATOR + 'configure.in')
+            File::foreach(file) do |line|
+              return true if line.lstrip.start_with?('GOBJECT_INTROSPECTION_CHECK')
+            end
+          end
+        end
+        false
+      end
+    end
+  end
+end
