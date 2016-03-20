9fad97199b1d6ef40baecb4110bee08dc9ce68cb
Daisuke Fujimura (fd0) <booleanlabel@gmail.com>
2016-03-20 14:37:16 +0900

    Added detector/halibut

---
+++ lib/xezat/detector/halibut.rb
@@ -0,0 +1,17 @@
+require 'find'
+require 'xezat/detectors'
+
+module Xezat
+  module Detector
+    class Halibut
+      DetectorManager::register(:halibut, self)
+
+      def detect(variables)
+        Find::find(variables[:S]) do |file|
+          return true if file.end_with?('.but')
+        end
+        false
+      end
+    end
+  end
+end
