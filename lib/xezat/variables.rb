c3a9b1ba9d734ee776d7b207889d4a114b081229
Daisuke Fujimura (fd0) <booleanlabel@gmail.com>
2019-01-05 18:02:40 +0900

    add cache

--- lib/xezat/variables.rb
+++ lib/xezat/variables.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'facets/file/atomic_write'
 require 'facets/string/word_wrap'
 require 'open3'
 require 'uri'
@@ -8,7 +9,18 @@ require 'xezat'
 
 module Xezat
   def variables(cygport)
+    cygport_dir = File.dirname(File.absolute_path(cygport))
+    cache_file = File.expand_path(File.join(cygport_dir, File.basename(cygport, '.cygport') + '.cache.yml'))
+
     LOG.debug('Extract variables')
+
+    if File.exist?(cache_file) && File.ctime(cache_file) > File.ctime(cygport)
+      LOG.debug('  Read cache for variables')
+      return YAML.safe_load(File.open(cache_file), [Symbol]).each_value do |v|
+        v.strip! if v.respond_to?(:strip)
+      end
+    end
+
     command = ['bash', File.expand_path(File.join(DATA_DIR, 'show_cygport_variable.sh')), cygport]
     result, error, status = Open3.capture3(command.join(' '))
     raise CygportProcessError, error unless status.success?
@@ -19,6 +31,12 @@ module Xezat
       v.strip! if v.respond_to?(:strip)
     end
     variables[:DESCRIPTION].word_wrap!(79)
+
+    File.atomic_write(cache_file) do |f|
+      LOG.debug('  Write cache for variables')
+      f.write(YAML.dump(variables))
+    end
+
     variables
   end
 end
