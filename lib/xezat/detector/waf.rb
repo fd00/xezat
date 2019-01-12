8490ab93c3bd80a48936bf5ee9d7ffa28faadf5c
Daisuke Fujimura (fd0) <booleanlabel@gmail.com>
2019-01-12 17:05:11 +0900

    fix waf

--- lib/xezat/detector/waf.rb
+++ lib/xezat/detector/waf.rb
@@ -4,7 +4,13 @@ module Xezat
   module Detector
     class Waf
       def detect(variables)
-        variables.key?(:_waf_CYGCLASS_)
+        if variables.key?(:_waf_CYGCLASS_)
+          Find.find(variables[:S]) do |file|
+            return false if file.end_with?(File::SEPARATOR + 'waf')
+          end
+          return true
+        end
+        false
       end
     end
   end
--- spec/detector/waf_spec.rb
+++ spec/detector/waf_spec.rb
@@ -4,12 +4,20 @@ require 'spec_helper'
 require 'xezat/detector/waf'
 
 describe Xezat::Detector::Waf do
-  it 'inherits waf' do
+  it 'contains waf' do
+    tmpdir = Dir.mktmpdir
+    FileUtils.touch(File.expand_path(File.join(tmpdir, 'waf')))
     detector = Xezat::Detector::Waf.new
-    expect(detector.detect(_waf_CYGCLASS_: '1')).to be_truthy
+    expect(detector.detect(S: tmpdir, _waf_CYGCLASS_: '1')).to be_falsey
+  end
+  it 'inherits waf' do
+    tmpdir = Dir.mktmpdir
+    detector = Xezat::Detector::Waf.new
+    expect(detector.detect(S: tmpdir, _waf_CYGCLASS_: '1')).to be_truthy
   end
   it 'does not inherit waf' do
+    tmpdir = Dir.mktmpdir
     detector = Xezat::Detector::Waf.new
-    expect(detector.detect({})).to be_falsey
+    expect(detector.detect(S: tmpdir)).to be_falsey
   end
 end
