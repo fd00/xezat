4e6016cef76255004e2b160736cb5f3c27bc333e
Daisuke Fujimura (fd0) <booleanlabel@gmail.com>
2019-12-25 11:53:32 +0900

    Store test results (CI)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,9 +25,4 @@ module Xezat
   # Xezat.logger = Logger.new('/dev/null')
 end
 
-if ENV['CIRCLE_ARTIFACTS']
-  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
-  SimpleCov.coverage_dir(dir)
-end
-
 SimpleCov.start
