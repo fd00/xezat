b667f034256fb2eb24916f07ec884df319e910c3
Daisuke Fujimura (fd0) <booleanlabel@gmail.com>
2019-12-28 11:02:47 +0900

    Rubocop

--- lib/xezat/command/bump/cygport_dep.rb
+++ lib/xezat/command/bump/cygport_dep.rb
@@ -8,9 +8,10 @@ module Xezat
   module Command
     class Bump
       def invoke_cygport_dep(vars, cygport)
-        additional_path = Find.find(vars[:D]).select do |file|
-          file.end_with?('.exe', '.dll')
-        end.map do |file|
+        candidate_files = Find.find(vars[:D]).select do |file|
+          file.end_with?('.exe', '.dll', '.so')
+        end
+        additional_path = candidate_files.map do |file|
           File.dirname(file)
         end.sort.uniq.join(':')
         command = ['bash', File.expand_path(File.join(DATA_DIR, 'invoke_cygport_dep.sh')), cygport]
