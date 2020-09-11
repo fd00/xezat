2b08f6032601548434e5b7546ec48b33d298d6dd
Daisuke Fujimura (fd0) <booleanlabel@gmail.com>
2020-09-11 20:16:34 +0900

    Refactoring

--- spec/command/bump/language_spec.rb
+++ spec/command/bump/language_spec.rb
@@ -7,9 +7,28 @@ require 'xezat/command/bump/language'
 
 describe Xezat::Command::Bump do
   include Xezat
-  it 'contains fortran & cpp' do
+  it 'contains C++' do
     tmpdir = Dir.mktmpdir
     FileUtils.touch(File.expand_path(File.join(tmpdir, 'test.cpp')))
+    command = Xezat::Command::Bump.new(nil, nil)
+    languages = command.get_languages(tmpdir)
+    expect(languages).to contain_exactly('C++')
+  end
+  it 'contains Fortran77' do
+    tmpdir = Dir.mktmpdir
+    File.atomic_write(File.expand_path(File.join(tmpdir, 'test.f77'))) do |f|
+      f.write(<<~FORTRAN77)
+        PROGRAM HELLO
+        PRINT *, 'HELLO, WORLD!'
+        END PROGRAM HELLO
+      FORTRAN77
+    end
+    command = Xezat::Command::Bump.new(nil, nil)
+    languages = command.get_languages(tmpdir)
+    expect(languages).to contain_exactly('Fortran')
+  end
+  it 'contains Fortran90' do
+    tmpdir = Dir.mktmpdir
     File.atomic_write(File.expand_path(File.join(tmpdir, 'test.f90'))) do |f|
       f.write(<<FORTRAN)
         program hello
@@ -17,6 +36,12 @@ describe Xezat::Command::Bump do
         end program hello
 FORTRAN
     end
+    command = Xezat::Command::Bump.new(nil, nil)
+    languages = command.get_languages(tmpdir)
+    expect(languages).to contain_exactly('Fortran Free Form')
+  end
+  it 'contains Protocol Buffer' do
+    tmpdir = Dir.mktmpdir
     File.atomic_write(File.expand_path(File.join(tmpdir, 'test.proto'))) do |f|
       f.write(<<PROTO)
         message Hello {}
@@ -24,6 +49,6 @@ PROTO
     end
     command = Xezat::Command::Bump.new(nil, nil)
     languages = command.get_languages(tmpdir)
-    expect(languages).to contain_exactly('C++', 'Fortran Free Form', 'Protocol Buffer')
+    expect(languages).to contain_exactly('Protocol Buffer')
   end
 end
