a1236eee1990514ae8db24bf0128a97f877afd13
Daisuke Fujimura (fd0) <booleanlabel@gmail.com>
2021-02-08 13:02:43 +0900

    Fix processing changelog

--- lib/xezat/command/bump/changelog.rb
+++ lib/xezat/command/bump/changelog.rb
@@ -9,7 +9,7 @@ module Xezat
   module Command
     class Bump
       def get_changelog(variables, options, readme_file)
-        Xezat.logger.debug('  Append latest log to changelog')
+        Xezat.logger.debug('  Try to append latest log to changelog...')
         current_version = variables[:PVR].intern
         if FileTest.exist?(readme_file)
           raise FilePermissionError, "Cannot read #{readme_file}" unless FileTest.readable?(readme_file)
@@ -17,10 +17,16 @@ module Xezat
 
           changelog = Cygchangelog.new(File.read(readme_file))
           message = options['message'] || 'Version bump.'
-          changelog[current_version] = message if changelog.length > 1 # overwrite unless initial package
+          if changelog.length > 1 || !changelog.key?(current_version)
+            changelog[current_version] = message # overwrite unless initial package
+            Xezat.logger.debug("    '#{message}' appended")
+          else
+            Xezat.logger.warn('    Initial release protected')
+          end
         else
           changelog = Cygchangelog.new
           changelog[current_version] = 'Initial release by fd0 <https://github.com/fd00/>'
+          Xezat.logger.debug('    Initial release by you')
         end
         changelog
       end
