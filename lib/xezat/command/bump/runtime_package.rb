e3a41a99ba3fcb6804ed949a5c9db9428ec2111f
Daisuke Fujimura (fd0) <booleanlabel@gmail.com>
2021-03-04 13:51:59 +0900

    Reflect ENV[:REQUIRES] for README

--- lib/xezat/command/bump/runtime_package.rb
+++ lib/xezat/command/bump/runtime_package.rb
@@ -7,10 +7,14 @@ require 'xezat/variables'
 module Xezat
   module Command
     class Bump
-      def get_runtime_packages(vars, cygport)
+      def get_runtime_packages(vars, pkgs, cygport)
         Xezat.logger.debug('  Collect runtime packages from cygport dep')
         result = invoke_cygport_dep(vars, cygport)
-        result.gsub(/^.*\*\*\*.*$/, '').split($INPUT_RECORD_SEPARATOR).map(&:lstrip)
+        runtime_packages = result.gsub(/^.*\*\*\*.*$/, '').split($INPUT_RECORD_SEPARATOR).map(&:lstrip)
+        vars[:REQUIRES]&.split&.each do |req|
+          runtime_packages << pkgs[req.to_sym]
+        end
+        runtime_packages.sort
       end
     end
   end
