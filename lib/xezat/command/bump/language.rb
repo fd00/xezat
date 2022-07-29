b712b5078c8b8725d58728f64461727077c8bff1
Daisuke Fujimura (fd0) <booleanlabel@gmail.com>
2022-07-29 11:38:58 +0900

    languages.json -> languages.yaml

--- lib/xezat/command/bump/language.rb
+++ lib/xezat/command/bump/language.rb
@@ -8,8 +8,8 @@ module Xezat
     class Bump
       def get_languages(top_src_dir)
         Xezat.logger.debug('  Collect languages')
-        languages_file = File.expand_path(File.join(DATA_DIR, 'languages.json'))
-        languages_candidates = JSON.parse(File.read(languages_file))
+        languages_file = File.expand_path(File.join(DATA_DIR, 'languages.yaml'))
+        languages_candidates = YAML.safe_load(File.open(languages_file), [Symbol])
         languages = []
         Find.find(top_src_dir) do |path|
           next if FileTest.directory?(path)
