5078246b4b8f05b72b29713c7acdc04cf3fe2a81
Daisuke Fujimura (fd0) <booleanlabel@gmail.com>
2023-12-27 09:04:42 +0900

    Split methods

--- lib/xezat/command/validate/config.rb
+++ lib/xezat/command/validate/config.rb
@@ -11,33 +11,14 @@ module Xezat
           Xezat.logger.debug("    #{File.basename(config)} found")
 
           begin
-            result, _, status = Open3.capture3("#{config} --version")
-            if status.success?
-              modversion = result.strip
-              Xezat.logger.debug("      modversion = #{modversion}")
-              pv = variables[:PV][0].gsub(/\+.+$/, '')
-              Xezat.logger.error("        modversion differs from $PN = #{pv}") unless modversion == pv
-            else
-              Xezat.logger.warn('       modversion not supported')
-            end
-          rescue StandardError => e
-            Xezat.logger.warn("       #{config} not executable: #{e}")
+            executable?(config)
+          rescue StandardError
             next
           end
 
-          result, _, status = Open3.capture3("#{config} --cflags")
-          if status.success?
-            Xezat.logger.debug("      cflags = #{result.strip}")
-          else
-            Xezat.logger.warn('       cflags not supported')
-          end
-
-          result, _, status = Open3.capture3("#{config} --cxxflags")
-          if status.success?
-            Xezat.logger.debug("      cxxflags = #{result.strip}")
-          else
-            Xezat.logger.warn('       cxxflags not supported')
-          end
+          validate_modversion(config, variables)
+          validate_cflags(config)
+          validate_cxxflags(config)
 
           result, _, status = Open3.capture3("#{config} --libs")
           if status.success?
@@ -46,6 +27,45 @@ module Xezat
           else
             Xezat.logger.warn('       libs not supported')
           end
+        end
+      end
+
+      private
+
+      def executable?(config)
+        Open3.capture3(config.to_s)
+      rescue StandardError => e
+        Xezat.logger.warn("       #{config} not executable: #{e}")
+        raise e
+      end
+
+      def validate_modversion(config, variables)
+        result, _, status = Open3.capture3("#{config} --version")
+        if status.success?
+          modversion = result.strip
+          Xezat.logger.debug("      modversion = #{modversion}")
+          pv = variables[:PV][0].gsub(/\+.+$/, '')
+          Xezat.logger.error("        modversion differs from $PN = #{pv}") unless modversion == pv
+        else
+          Xezat.logger.warn('       modversion not supported')
+        end
+      end
+
+      def validate_cflags(config)
+        result, _, status = Open3.capture3("#{config} --cflags")
+        if status.success?
+          Xezat.logger.debug("      cflags = #{result.strip}")
+        else
+          Xezat.logger.warn('       cflags not supported')
+        end
+      end
+
+      def validate_cxxflags(config)
+        result, _, status = Open3.capture3("#{config} --cxxflags")
+        if status.success?
+          Xezat.logger.debug("      cxxflags = #{result.strip}")
+        else
+          Xezat.logger.warn('       cxxflags not supported')
         end
       end
     end
