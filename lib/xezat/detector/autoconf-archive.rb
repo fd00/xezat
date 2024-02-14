1fc131a6459e46f37c5989ee5174bae9fa54d232
Daisuke Fujimura (fd0) <booleanlabel@gmail.com>
2024-02-14 21:36:32 +0900

    Don't detect autoconf-archive unless autotools is used

--- lib/xezat/detector/autoconf-archive.rb
+++ lib/xezat/detector/autoconf-archive.rb
@@ -4,6 +4,10 @@ module Xezat
   module Detector
     class AutoconfArchive
       def detect(variables)
+        return false unless variables.keys.index do |key|
+          %i[_cmake_CYGCLASS_ _meson_CYGCLASS_ _ninja_CYGCLASS_].include?(key)
+        end.nil?
+
         Find.find(variables[:S]) do |file|
           next unless file.end_with?("#{File::SEPARATOR}configure.ac", "#{File::SEPARATOR}configure.in")
 
