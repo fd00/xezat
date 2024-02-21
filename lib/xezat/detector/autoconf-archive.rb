9d872732b5a0f51b6ceadebde72e21ba19d16d88
Daisuke Fujimura (fd0) <booleanlabel@gmail.com>
2024-02-21 12:32:18 +0900

    Detect autoconf-archive strictly

--- lib/xezat/detector/autoconf-archive.rb
+++ lib/xezat/detector/autoconf-archive.rb
@@ -8,11 +8,16 @@ module Xezat
           %i[_cmake_CYGCLASS_ _meson_CYGCLASS_ _ninja_CYGCLASS_].include?(key)
         end.nil?
 
+        autoconf_archive_file = File.expand_path(File.join(DATA_DIR, 'autoconf-archive.yml'))
+        autoconf_archive_macros = YAML.safe_load(File.open(autoconf_archive_file), symbolize_names: true, permitted_classes: [Symbol])
+
         Find.find(variables[:S]) do |file|
           next unless file.end_with?("#{File::SEPARATOR}configure.ac", "#{File::SEPARATOR}configure.in")
 
-          File.foreach(file) do |line|
-            return true if line.strip.start_with?('AX_')
+          autoconf_archive_macros.each do |macro|
+            File.foreach(file) do |line|
+              return true if line.strip.start_with?(macro)
+            end
           end
         end
         false
--- spec/detector/autoconf-archive_spec.rb
+++ spec/detector/autoconf-archive_spec.rb
@@ -7,15 +7,23 @@ require 'tmpdir'
 require 'xezat/detector/autoconf-archive'
 
 describe Xezat::Detector::AutoconfArchive do
-  it 'contains AX_* in configure.ac' do
+  it 'contains autoconf-archive macro in configure.ac' do
     tmpdir = Dir.mktmpdir
     File.atomic_write(File.expand_path(File.join(tmpdir, 'configure.ac'))) do |f|
-      f.puts('AX_COMPILER_FLAGS(')
+      f.puts('AX_COMPILER_FLAGS')
     end
     detector = Xezat::Detector::AutoconfArchive.new
     expect(detector.detect(S: tmpdir)).to be_truthy
   end
-  it 'contains no AX_* in configure.ac' do
+  it 'contains not autoconf-archive macro in configure.ac' do
+    tmpdir = Dir.mktmpdir
+    File.atomic_write(File.expand_path(File.join(tmpdir, 'configure.ac'))) do |f|
+      f.puts('AX_CPP_VARARG_MACRO_ISO')
+    end
+    detector = Xezat::Detector::AutoconfArchive.new
+    expect(detector.detect(S: tmpdir)).to be_falsey
+  end
+  it 'contains no autoconf-archive macro in configure.ac' do
     tmpdir = Dir.mktmpdir
     File.atomic_write(File.expand_path(File.join(tmpdir, 'configure.ac'))) do |f|
       f.puts('AC_INIT(')
