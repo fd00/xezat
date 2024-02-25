a176e9fdf3570a36fc3958ae1eeb14c772257051
Daisuke Fujimura (fd0) <booleanlabel@gmail.com>
2024-02-25 09:53:02 +0900

    Detect xorg-util-macros

---
+++ lib/xezat/detector/xorg-util-macros.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'find'
+
+module Xezat
+  module Detector
+    class XorgUtilMacros
+      def detect(variables)
+        Find.find(variables[:S]) do |file|
+          next unless file.end_with?("#{File::SEPARATOR}configure.ac", "#{File::SEPARATOR}configure.in")
+
+          File.foreach(file) do |line|
+            return true if line.start_with?('XORG_MACROS_VERSION')
+          end
+        end
+        false
+      end
+    end
+  end
+end
---
+++ spec/detector/xorg-util-macros_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'facets/file/atomic_write'
+require 'fileutils'
+require 'spec_helper'
+require 'tmpdir'
+require 'xezat/detector/xorg-util-macros'
+
+describe Xezat::Detector::XorgUtilMacros do
+  it 'contains xorg-util-macros' do
+    tmpdir = Dir.mktmpdir
+    File.atomic_write(File.expand_path(File.join(tmpdir, 'configure.ac'))) do |f|
+      f.puts('XORG_MACROS_VERSION')
+    end
+    detector = Xezat::Detector::XorgUtilMacros.new
+    expect(detector.detect(S: tmpdir)).to be_truthy
+  end
+  it 'contains no xorg-util-macros' do
+    tmpdir = Dir.mktmpdir
+    FileUtils.touch(File.expand_path(File.join(tmpdir, 'configure.ac')))
+    detector = Xezat::Detector::XorgUtilMacros.new
+    expect(detector.detect(S: tmpdir)).to be_falsey
+  end
+end
